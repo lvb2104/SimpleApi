b58950bba5cc68fc2563565a91508797631c7db6
Bao Le <levanbao2105@gmail.com>
2025-08-06 20:27:51 +0700

    Add project files.

---
+++ DevOpsAPI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using DevOpsAPI.Controllers;
+
+namespace DevOpsAPI.Tests
+{
+    public class UnitTest1
+    {
+        private readonly WeatherForecastController _controller = new WeatherForecastController();
+
+        [Fact]
+        public void Test1()
+        {
+            var result = _controller.Get();
+            Assert.NotNull(result);
+        }
+    }
+}
